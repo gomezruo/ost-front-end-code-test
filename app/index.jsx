e4b70bdb17c9158ce48cdb301efea2580c6f919a
gomezruo <georgejjrichards@gmail.com>
2018-05-21 22:49:59 +0100

    structuring app and linking components and utilities together

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import styles from './index.scss';
+import {PageSection} from './components/pageSection/pageSection';
+import {Season} from './components/season/season';
 
-import './index.global.scss';
+import './index.scss';
 
 render(
-  <div className={styles['example-class']}>
-    Ostmodern Front-end Code Test
-  </div>,
+  <PageSection heading="Season">
+    <Season seasonKey="seas_e85496eb48df4225b9d9f3fde1010398" />
+  </PageSection>,
   document.getElementById('root')
 );
